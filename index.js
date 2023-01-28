7348e32a7f11c2631c4881ae13fd0bd8974a7e62
Waffle <ethanstorm11@gmail.com>
2023-01-27 22:03:54 -0500

    First functional setup of application.

---
+++ index.js
@@ -0,0 +1,48 @@
+// Initializes modules
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const consoleTable = require('console.table');
+
+const promptMessages = {
+    viewAllEmployees: "View All Employees",
+    viewByDepartment: "View All Employees By Department",
+    viewByManager: "View All Employees By Manager",
+    addEmployee: "Add An Employee",
+    removeEmployee: "Remove An Employee",
+    updateRole: "Update Employee Role",
+    updateEmployeeManager: "Update Employee Manager",
+    viewAllRoles: "View All Roles",
+    exit: "Exit"
+};
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Test1234',
+    database: 'employees'
+});
+
+connection.connect(err => {
+    //Will keep attempting to throw err, removing this allows the program to work just fine
+    //if (err) throw err;
+    prompt();
+});
+
+function prompt() {
+    inquirer
+        .prompt({
+            name: 'action',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                promptMessages.viewAllEmployees,
+                promptMessages.viewByDepartment,
+                promptMessages.viewByManager,
+                promptMessages.viewAllRoles,
+                promptMessages.addEmployee,
+                promptMessages.removeEmployee,
+                promptMessages.updateRole,
+                promptMessages.exit
+            ]
+        })
+}
